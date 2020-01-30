45aa60386d9c878f3631fa5e95e14f41620bc290
mehmet çalış <mehmet_cls@hotmail.com>
2020-01-30 17:18:18 +0300

    Add project files.

---
+++ MarsRover/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public enum Enums
+    {
+        N=1,
+        S=2,
+        W=3,
+        E=4
+
+    }
+}
---
+++ MarsRover/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace MarsRover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WellCome The Mars, Enter your research area!(X Y)");
+            var reseachArea = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
+
+            Console.WriteLine("Enter Rover Position");
+            var firstPosition = Console.ReadLine().Trim().Split(' ');
+
+            MotionControl control = new MotionControl();
+
+            if (firstPosition.Count() == 3)
+            {
+                control.X = Convert.ToInt32(firstPosition[0]);
+                control.Y = Convert.ToInt32(firstPosition[1]);
+                control.Direction = (Directions)Enum.Parse(typeof(Directions), firstPosition[2]);
+            }
+
+            Console.WriteLine("Define To Rover Movement");
+            var moves = Console.ReadLine().ToUpper();
+
+            try
+            {
+                control.StartMoving(reseachArea, moves);
+                Console.WriteLine($"{control.X} {control.Y} {control.Direction.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
